4c7de8b8628378a32a88142231bd5863db2ba8b2
Theofilos <theofilos_xri@yahoo.gr>
2022-06-02 12:41:36 +0300

    Reverting changes on purposefully failing test

--- tests/Interpolation/Extrapolation/GaussLegendeExtrapolation3x3.cs
+++ tests/Interpolation/Extrapolation/GaussLegendeExtrapolation3x3.cs
@@ -15,22 +15,21 @@ namespace MGroup.MSolve.Numerics.Tests.Interpolation.Extrapolation
 		[Fact]
 		private static void ExtrapolateToGaussPoints()
 		{
-			Assert.True(false);
-			//double tolerance = 1e-10;
-			//var extrapolation = ExtrapolationGaussLegendre3x3.UniqueInstance;
-			//var gaussPoints = GaussLegendre2D.GetQuadratureWithOrder(3, 3).IntegrationPoints;
+			double tolerance = 1e-10;
+			var extrapolation = ExtrapolationGaussLegendre3x3.UniqueInstance;
+			var gaussPoints = GaussLegendre2D.GetQuadratureWithOrder(3, 3).IntegrationPoints;
 
-			//for (int i = 0; i < gaussPoints.Count; ++i)
-			//{
-			//	var scalarsAtGPs = new double[9];
-			//	scalarsAtGPs[i] = 1.0;
-			//	for (int j = 0; j < gaussPoints.Count; ++j)
-			//	{
-			//		double extrapolatedScalar = extrapolation.ExtrapolateScalarFromGaussPoints(scalarsAtGPs, gaussPoints[j]);
-			//		if (j == i) Assert.True(Utilities.AreValuesEqual(1.0, extrapolatedScalar, tolerance));
-			//		else Assert.True(Utilities.AreValuesEqual(0.0, extrapolatedScalar, tolerance));
-			//	}
-			//}
+			for (int i = 0; i < gaussPoints.Count; ++i)
+			{
+				var scalarsAtGPs = new double[9];
+				scalarsAtGPs[i] = 1.0;
+				for (int j = 0; j < gaussPoints.Count; ++j)
+				{
+					double extrapolatedScalar = extrapolation.ExtrapolateScalarFromGaussPoints(scalarsAtGPs, gaussPoints[j]);
+					if (j == i) Assert.True(Utilities.AreValuesEqual(1.0, extrapolatedScalar, tolerance));
+					else Assert.True(Utilities.AreValuesEqual(0.0, extrapolatedScalar, tolerance));
+				}
+			}
 		}
 
 		private static void ExtrapolateToNodes()
