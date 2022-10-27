b9ff04bcb552e0f5d6b446180d6a64872f6c4818
TheoChristo <theofilos_xri@yahoo.gr>
2022-10-27 15:07:54 +0200

    Adding linear system rhs property to IChildAnalyzer

--- src/AnalysisWorkflow/IChildAnalyzer.cs
+++ src/AnalysisWorkflow/IChildAnalyzer.cs
@@ -1,7 +1,11 @@
+using MGroup.MSolve.Solution.LinearSystem;
+
 namespace MGroup.MSolve.AnalysisWorkflow
 {
 	public interface IChildAnalyzer : IAnalyzer
 	{
 		IParentAnalyzer ParentAnalyzer { get; set; }
+
+		public IGlobalVector CurrentAnalysisLinearSystemRhs { get; }
 	}
 }
