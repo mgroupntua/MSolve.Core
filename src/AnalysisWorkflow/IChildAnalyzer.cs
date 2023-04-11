4c576935f86368f6a8b296d56034b438a0360af6
George Stavroulakis <stavroulakis@nessos.gr>
2023-04-11 17:26:33 +0300

    Added AnalysisStatistics at IParentAnalyzer and IChildAnalyzer

--- src/AnalysisWorkflow/IChildAnalyzer.cs
+++ src/AnalysisWorkflow/IChildAnalyzer.cs
@@ -1,9 +1,11 @@
+using MGroup.LinearAlgebra.Iterative;
 using MGroup.MSolve.Solution.LinearSystem;
 
 namespace MGroup.MSolve.AnalysisWorkflow
 {
 	public interface IChildAnalyzer : IAnalyzer
 	{
+		IterativeStatistics AnalysisStatistics { get; }
 		IParentAnalyzer ParentAnalyzer { get; set; }
 
 		public IGlobalVector CurrentAnalysisLinearSystemRhs { get; }
--- src/AnalysisWorkflow/IParentAnalyzer.cs
+++ src/AnalysisWorkflow/IParentAnalyzer.cs
@@ -1,7 +1,13 @@
 namespace MGroup.MSolve.AnalysisWorkflow
 {
+	using System.Collections;
+	using System.Collections.Generic;
+
+	using MGroup.LinearAlgebra.Iterative;
+
 	public interface IParentAnalyzer : IAnalyzer
 	{
+		IList<IterativeStatistics> AnalysisStatistics { get; }
 		IChildAnalyzer ChildAnalyzer { get; }
 		void BuildMatrices();
 	}
